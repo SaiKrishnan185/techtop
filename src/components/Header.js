5020f657031c601bc983c5099c6b319b8e439c5b
Sai Krishnan RU <saikrishnan185@gmail.com>
2023-06-06 00:35:49 +0530

    Changed color

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,7 +23,7 @@ export const Header = () => {
       <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
     </button>
     <div className={` ${hidden ? "hidden" : ""}  w-full md:block md:w-auto`} id="navbar-solid-bg">
-      <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+      <ul className="flex flex-col font-medium mt-4 rounded-lg bg-black md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
         <li>
           <NavLink to="/" className={({isActive}) => isActive ? activeClass : inActiveClass} aria-current="page">Home</NavLink>
         </li>
