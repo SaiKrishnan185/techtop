2ebae8e07a328a99858b29bc94810da873a1dfde
Sai Krishnan RU <saikrishnan185@gmail.com>
2023-06-27 00:40:46 +0530

    Fixed background video spanning issue

--- src/components/homePage/ServiceCard.js
+++ src/components/homePage/ServiceCard.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 export const ServiceCard = ({head, detail, serviceLink, card}) => {
   return (
     <div className=' rounded-2xl border-l border-r border-b md:max-w-md w-full h-full  mb-2 '>  
-          <img class="rounded-t-2xl  w-full h-72 bg-contain" src={card} alt="" />
+          <img className="rounded-t-2xl  w-full h-72 bg-contain" src={card} alt="" />
           <div className="p-5 flex flex-col text-center items-center justify-center  rounded-b-2xl">
               <h5 className="mb-2 md:h-8 text-2xl font-bold tracking-tight text-white">{head}</h5>
               <p className="mb-3 md:h-36  text-sm text-gray-500 tracking-tight">{detail}</p>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,13 @@
 import { ConnectPage, Industry, Intro, Service, TechPage } from "../components"
 
+import bgTest from "../assets/bgtest.mp4"
+
 export const Home = ({serviceList}) => {
   return (
-    <div>
+    <div className="">
+        <video autoPlay muted loop className="background-video h-screen">
+        <source src={bgTest} type="video/mp4" />
+        </video>
         <Intro />
         <Service/>
         <Industry />
